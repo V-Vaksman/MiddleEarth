8e96cf041508e50fc5e88a78b556d6bbcfe4cbb3
vvaksman <vvaksman@diasoft.ru>
2018-11-12 12:10:19 +0500

    Реконструкция кода
    - создан метод Duel
    - изменены Round, Punch

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import middleearth.*;
 //import static middleearth.MiddleEarthCitizen.getCountryOrigin;
 
 public class Main {
 
-    public static int RandBetween(int min, int max){
+    public static int RandBetween(int min, int max) // Генерация случайного целого числа от min до max
+    {
         Random r = new Random();
         return min + r.nextInt(max - min + 1);
     }
 
     public static void main(String[] args) {
+        // Размеры армий генерируются в этих пределах
         final int maxCount = 12;
         final int minCount = 8;
 
@@ -22,12 +25,16 @@ public class Main {
         int lightCount = RandBetween(minCount,maxCount);
         int darkCount = RandBetween(minCount,maxCount);
 
+        System.out.println("И началась последняя битва Средиземья");
+
+        // Случайное добавление чародея
         if (r.nextBoolean()) {
             Light.cavalry.add(new Wizard());
-            System.out.println("Светлая армия заручилась поддержкой чародея!");
+            System.out.println("\nСветлая армия заручилась поддержкой чародея!");
             lightCount--;
         }
 
+        // Случайное заполнение армий
         for(int i = 0; i < lightCount; i++){
             int elem = r.nextInt(4);
             switch (elem) {
@@ -45,7 +52,6 @@ public class Main {
                     break;
             }
         }
-
         for(int i = 0; i < darkCount; i++){
             int elem = r.nextInt(4);
             switch (elem) {
@@ -64,71 +70,89 @@ public class Main {
             }
         }
 
-        System.out.println("СВЕТЛАЯ АРМИЯ ("+(Light.cavalry.size()+Light.infantry.size())+"):");
+        System.out.println("*** СВЕТЛАЯ АРМИЯ ("+(Light.cavalry.size()+Light.infantry.size())+") ***");
         Light.Print();
-        System.out.println("\nТЕМНАЯ АРМИЯ ("+(Dark.cavalry.size()+Dark.infantry.size())+"):");
+        System.out.println("\n*** ТЕМНАЯ АРМИЯ ("+(Dark.cavalry.size()+Dark.infantry.size())+") ***");
         Dark.Print();
 
         System.out.println("\nРаунд 1 - всадники");
         if (Light.cavalry.size()>0 && Dark.cavalry.size()>0)
-            Round(Light.cavalry, Dark.cavalry,0);
+            Round(Light.cavalry, Dark.cavalry, false); // Всадники сражаются со всадниками
+        else System.out.println("\nРаунд не состоялся");
 
         System.out.println("\nРаунд 2 - пехота");
         if (Light.infantry.size()>0 && Dark.infantry.size()>0)
-            Round(Light.infantry, Dark.infantry,0);
+            Round(Light.infantry, Dark.infantry, false); // Пехотинцы сражаются с пехотинцами
+        else System.out.println("\nРаунд не состоялся");
 
         if (Light.cavalry.size()>0 && Light.infantry.size()>0)
             System.out.println("\nПобедила светлая армия! "+(Light.cavalry.size()+Light.infantry.size())+" выживших");
         else if (Dark.cavalry.size()>0 && Dark.infantry.size()>0)
             System.out.println("\nПобедила темная армия! "+(Dark.cavalry.size()+Dark.infantry.size())+" выживших");
         else {
-            System.out.println("\nРаунд 3 - недобитки");
+            System.out.println("\nРаунд 3 - решающий бой");
             if (Light.cavalry.size()>0)
-                Round(Light.cavalry,Dark.infantry,1);
+                Round(Light.cavalry, Dark.infantry, true); // Выжившие всадники сражаются с выжившими пехотинцами
             else
-                Round(Light.infantry,Dark.cavalry,2);
-            if (Light.infantry.size()>0 || Light.cavalry.size()>0) System.out.println("\nСветлая армия в решающей схватке одержала верх! "+(Light.cavalry.size()+Light.infantry.size())+" выживших");
+                Round(Dark.cavalry, Light.infantry,true);
+            if (Light.infantry.size()>0 || Light.cavalry.size()>0)
+                System.out.println("\nСветлая армия в решающей схватке одержала верх! "+(Light.cavalry.size()+Light.infantry.size())+" выживших");
             else System.out.println("\nТемная армия в решающей схватке одержала верх! "+(Dark.cavalry.size()+Dark.infantry.size())+" выживших");
         }
     }
 
-    public static void Round(ArrayList<MiddleEarthCitizen> Light, ArrayList<MiddleEarthCitizen> Dark, int PriorNum){
+    // в Round, Duel и Punch - "первый" и "второй" не связаны, это локальные обозначения
+
+    public static void Round(ArrayList<MiddleEarthCitizen> first, ArrayList<MiddleEarthCitizen> second, boolean firstPrior) // Раунд битвы
+    {
         Random r = new Random();
         do {
-            int goodGuy = RandBetween(0,Light.size()-1);
-            int goodPow = Light.get(goodGuy).getPower();
-            String goodName = Light.get(goodGuy).getRace();
-            int badGuy = RandBetween(0,Dark.size()-1);
-            int badPow = Dark.get(badGuy).getPower();
-            String badName = Dark.get(badGuy).getRace();
-            boolean enough = false;
-            System.out.println("\n"+goodName+" ("+goodPow+") vs "+badName+" ("+badPow+")");
+            // выбор случайного воина
+            int firstNum = RandBetween(0,first.size()-1);
+            int secondNum = RandBetween(0,second.size()-1);
 
-            if (PriorNum == 1 || (r.nextBoolean() && PriorNum != 2)) { // Если приоритет у светлого или рандом и нет приоритета у темного
-                enough = Punch(goodPow, badGuy, badPow, badName, Dark); // Бьет светлый
+            if (firstPrior || r.nextBoolean()) { // Если приоритет у первой армии или рандом
+                Duel(first, second, firstNum, secondNum); // первый воин бьет первым
             }
-            while (!enough) {
-                badPow = Dark.get(badGuy).getPower();
-                enough = Punch(badPow, goodGuy, goodPow, goodName, Light); // Бьет темный
-                if (!enough) {
-                    goodPow = Light.get(goodGuy).getPower();
-                    enough = Punch(goodPow, badGuy, badPow, badName, Dark); // Бьет светлый
-                }
+            else {
+                Duel(second, first, secondNum, firstNum); // второй воин бьет первым
             }
-        } while (Light.size() > 0 && Dark.size() > 0);
+        } while (first.size() > 0 && second.size() > 0); // пока есть кому сражаться в каждой армии
     }
 
-    public static boolean Punch(int attackerPow, int defenderNum, int defenderPow, String defenderName, ArrayList<MiddleEarthCitizen> defenders){
+    public static void Duel(ArrayList<MiddleEarthCitizen> first, ArrayList<MiddleEarthCitizen> second, int firstNum, int secondNum) // циклический обмен ударами между первым и вторым воином
+    {
+        boolean enough = false;
+        int firstPow = first.get(firstNum).getPower();
+        int secondPow = second.get(secondNum).getPower();
+        String firstName = first.get(firstNum).getRace();
+        String secondName = second.get(secondNum).getRace();
+        System.out.println("\n" + firstName + "(" + firstPow + ") vs " + secondName + "(" + secondPow + ")");
+        while (!enough) // пока живы оба - начинает первый
+        {
+            firstPow = first.get(firstNum).getPower();
+            enough = Punch(firstPow, secondNum, secondPow, firstName, secondName, second); // Бьет первый
+            if (!enough) // если живы оба - отвечает второй
+            {
+                secondPow = second.get(secondNum).getPower();
+                enough = Punch(secondPow, firstNum, firstPow, secondName, firstName, first); // Бьет второй
+            }
+        }
+    }
+
+    public static boolean Punch(int attackerPow, int defenderNum, int defenderPow, String attackerName, String defenderName, ArrayList<MiddleEarthCitizen> defenders) // Удар первого по второму
+    {
         boolean exit = false;
-        if (defenderPow-attackerPow <= 0) {
-            System.out.println(defenderName+" в нокауте!");
+        if (defenderPow-attackerPow <= 0) // если обороняющийся не перенес удар
+        {
+            System.out.println(attackerName + "(" + attackerPow + "): " + defenderName + " в нокауте!");
             defenders.remove(defenderNum);
             exit = true;
         }
         else {
             defenderPow -= attackerPow;
             defenders.get(defenderNum).setPower(defenderPow);
-            System.out.println(defenderName+" -"+attackerPow+" HP");
+            System.out.println(attackerName + "(" + attackerPow + "): " + defenderName+" -"+attackerPow+" HP");
         }
         return exit;
     }
